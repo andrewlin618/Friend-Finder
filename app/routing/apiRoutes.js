f6a0972c4aa2e2f7839b392073c7cbbdf6f4d9de
Andrew Lin <andrewlin618@gmail.com>
2019-10-07 12:41:16 -0700

    done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,11 @@ router.get('/friends', function (req, res) {
 });
 
 router.post('/friends', function (req, res) {
+    var realScore = req.body.scores;
+    for (var i =0;i<realScore.length;i++){
+        realScore[i] = parseInt(realScore[i])
+    }
+    console.log(realScore);
     var user = {
         name: req.body.name,
         photo: req.body.photo,
@@ -34,6 +39,8 @@ router.post('/friends', function (req, res) {
             bestMatch = friendsList[i];
         }
     }
+
+    res.send(bestMatch);
     friendsList.push(user);
     console.log(friendsList);
     // fs.writeFile('../data/friends.js', friendsList, function (err) {
